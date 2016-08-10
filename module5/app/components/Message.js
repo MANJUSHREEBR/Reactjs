d493f13ace3e6ed9356faba4317e76aa31354d9d
MANJUSHREEBR <manjushree978@gmail.com>
2016-08-10 11:04:04 +0530

    fifth module in progress

---
+++ module5/app/components/Message.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
+
+var Message = React.createClass({
+  mixins: [PureRenderMixin],
+
+  render: function() {
+    return (
+      <p>{this.props.who} said: "{this.props.text}"</p>
+    )
+  }
+});
+export default Message;
